7bb39869f3959974ea233674b124d8ae913cb0f4
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-09-12 17:07:37 +0500

    refactor

--- design-patterns/command-pattern-without-react/stock-market/Client.ts
+++ design-patterns/command-pattern-without-react/stock-market/Client.ts
@@ -1,11 +1,17 @@
-import { Agent } from "./Agent";
+//* Concrete Commands
 import { BuyStockOrder } from "./BuyStockOrder";
 import { SellStockOrder } from "./SellStockOrder";
+
+//* To initialize Stocks
 import { StockTrade } from "./StockTrade";
+
+//* Receiver
+import { Agent } from "./Agent";
 
 const stock = new StockTrade();
 const buyStock = new BuyStockOrder(stock);
 const sellStock = new SellStockOrder(stock);
+
 const agent = new Agent();
 agent.placeOrder(buyStock);
 agent.placeOrder(sellStock);
--- design-patterns/command-pattern-without-react/stock-market/Order.ts
+++ design-patterns/command-pattern-without-react/stock-market/Order.ts
@@ -1,8 +1,8 @@
-const GENERATE_RANDOM_NUMBER = 10000;
+const MAX_UID_RANGE = 10000;
 
 //* Abstract Command Class/Object
 export abstract class Order {
-  protected uid = Math.floor(Math.random() * GENERATE_RANDOM_NUMBER);
+  protected uid = Math.floor(Math.random() * MAX_UID_RANGE);
   protected type!: string;
   public displayOrder(): string {
     return `UID: ${this.uid} - Operation: ${this.type}`;
